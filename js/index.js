95cc769f4a533cd5c222752c1d14ac370a587ab7
Yashasvi Chaurasia <yashasvi12977@gmail.com>
2022-09-11 05:36:14 +0530

    enabling default mode
    Signed-off-by: Yashasvi Chaurasia <yashasvi12977@gmail.com>

--- js/index.js
+++ js/index.js
@@ -160,6 +160,7 @@ let darkColor = "#111216";
     icon.onmouseout = () => {
             icon.setAttribute("src","images/sun.svg");
     }
+    
 switchDarkLightMode();
 displaySection("about-me","about-me-section");
 displaySection("skills","skills-section");
